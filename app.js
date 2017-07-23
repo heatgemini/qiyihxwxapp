38b8d60da81ebee827d73ced2931bc7d303742b6
张庆 <zhang_qing@suixingpay.com>
2017-07-23 17:15:09 +0800

    模版消息发送处理

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //app.js
 App({
+  data: {
+    requestUrl: "https://wxapi.hotapp.cn/proxy/?appkey=hotapp2427615&url=URL",
+    json:""
+  },
   onLaunch: function () {
-    console.log('test');
     var that = this;
     //  获取商城名称
     wx.request({
@@ -69,18 +72,18 @@ App({
         var code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
         wx.getUserInfo({
           success: function (res) {
-            console.log(res)
             var iv = res.iv;
             var encryptedData = res.encryptedData;
             // 下面开始调用注册接口
-            wx.request({
-              url: 'https://api.it120.cc/' + that.globalData.subDomain +'/user/wxapp/register/complex',
-              data: {code:code,encryptedData:encryptedData,iv:iv}, // 设置请求的 参数
-              success: (res) =>{
-                wx.hideLoading();
-                that.login();
-              }
-            })
+            //wx.request({
+              //url: 'https://api.it120.cc/' + that.globalData.subDomain +'/user/wxapp/register/complex',
+             // data: {code:code,encryptedData:encryptedData,iv:iv}, // 设置请求的 参数
+              //success: (res) =>{
+               // console.log();
+                //wx.hideLoading();
+                //that.login();
+              //}
+           // })
           }
         })
       }
@@ -107,5 +110,24 @@ App({
         }
       });
     }
+  },
+  sendTplMsg: function (formId, content) {
+    var that = this;
+    wx.login({
+      success: function (res) {
+        var code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
+        wx.request({
+          url: that.data.requestUrl.replace('URL', 'http://qiyihx.com/api/wxapp/get_openid.php?js_code=' + code + '&form_id=' + formId),
+          success: function (res) {
+            var result = res.data.result;
+            wx.showModal({
+              title: '提示',
+              content: result.errcode == 0 ? '发送成功' : '发送失败:' + result.errcode,
+              showCancel: false
+            })
+          }
+        })
+      }
+    });
   },
 })
--- pages/me/index.js
+++ pages/me/index.js
@@ -20,12 +20,12 @@ Page({
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
-      console.log(userInfo)
       //更新数据
       that.setData({
         userInfo: userInfo
       })
     })
+    app.registerUser()
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
@@ -46,5 +46,12 @@ Page({
   onReachBottom: function() {
     // 页面上拉触底事件的处理函数
   
+  },
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.formId)
+    app.sendTplMsg(e.detail.formId, '你从哪里了');
+  },
+  formReset: function(e) {
+    console.log('form发生了reset事件:', e.detail.formId)
   }
 })
